8c588a7a80fa0615b67ce26cc71d4fddcc985bdf
Aman Chopra <56465233+Aman1905@users.noreply.github.com>
2021-03-10 18:07:43 +0530

    updated whatsapp clone app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+  
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './Login';
+import { useStateValue } from './StateProvider';
+
 
 function App() {
+
+
+
+  const [{ user }, dispatch] = useStateValue();
+
   return (
+    // BEM naming convention
     <div className="app">
-      <h1>Whatsapp Clone</h1>
+
+      {!user ? (
+        <Login/>
+      ) : (
+          <div className="app_body">
+            <Router>
+              <Sidebar />
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <Chat />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,28 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+import firestore from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBRnjW466jE9A9aCqiwu4qLBmTLLWh05Kc",
-    authDomain: "whatsapp-clone-d2528.firebaseapp.com",
-    projectId: "whatsapp-clone-d2528",
-    storageBucket: "whatsapp-clone-d2528.appspot.com",
-    messagingSenderId: "667982133769",
-    appId: "1:667982133769:web:00e759b4cb9d22bd9a16d8",
-    measurementId: "G-NVGCMB5NT1"
-  };
+  apiKey: "AIzaSyBRnjW466jE9A9aCqiwu4qLBmTLLWh05Kc",
+  authDomain: "whatsapp-clone-d2528.firebaseapp.com",
+  projectId: "whatsapp-clone-d2528",
+  storageBucket: "whatsapp-clone-d2528.appspot.com",
+  messagingSenderId: "667982133769",
+  appId: "1:667982133769:web:00e759b4cb9d22bd9a16d8",
+  measurementId: "G-NVGCMB5NT1"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+firebase.firestore()
+
+const db = firebaseApp.firestore();
+// for database
+
+const auth = firestore.auth();
+// for firebase authentication
+
+const provider = new firebase.auth.GoogleAuthProvider();
+// for google authentication
+
+export {auth, provider};
+export default db;
